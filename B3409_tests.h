7fca63ad3ebbcae427eacf69fc7338ea11f71949
Karim <ikarim@me.com>
2013-10-20 19:12:30 +0200

    man tests de count

---
+++ B3409_tests.h
@@ -0,0 +1,19 @@
+#ifndef B3409_TESTS_H_
+#define B3409_TESTS_H_
+#include "Cartography.h"
+
+class B3409_tests
+{
+public:
+	B3409_tests();
+	virtual ~B3409_tests();
+	bool test1();
+	bool test2();
+	bool test3();
+	bool test4();
+	bool test5();
+	bool test6();
+	bool test7();
+};
+
+#endif /*B3409_TESTS_H_*/
